f1e8787d8e3aec5c76832a5ba6266f8f9c83151a
lasaga09 <lsg_stone@hotmail.com>
2020-01-21 17:43:24 -0500

    web

---
+++ js/about.31557253.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about"},[a("div",{staticClass:"m-4"}),a("h1",[t._v("This is an about page")])])}],i=a("2877"),u={},c=Object(i["a"])(u,n,s,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=about.31557253.js.map
